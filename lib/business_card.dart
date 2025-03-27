9add5acbf1afab5e99f990cecf5587653f5af47b
AbdulrahmanoTohamy <bidohussam@gmail.com>
2025-03-28 00:20:15 +0200

    add title inside appbar

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -6,7 +6,10 @@ class BusinessCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Business Card"),
+        centerTitle: true,
+      ),
     );
   }
 }
