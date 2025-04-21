5e1380b57727280b7b51ca1950dc41a77107dc1c
AbdulrahmanoTohamy <bidohussam@gmail.com>
2025-04-21 21:42:27 +0200

    add business card code

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -10,6 +10,53 @@ class BusinessCard extends StatelessWidget {
         title: Text("Business Card"),
         centerTitle: true,
       ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 20,
+            children: [
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage(
+                  "assets/WhatsApp Image 2024-09-23 at 18.13.38_03366202.jpg",
+                ),
+              ),
+              Text(
+                "Abdelrahman Hussam",
+                style: TextStyle(fontSize: 30),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 40,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  "01092003188",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 40,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  "abdelrahmanhussam008@gmail.com",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
